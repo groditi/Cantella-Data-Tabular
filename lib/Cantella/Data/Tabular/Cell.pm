8ddb254ecfe6c774cd21da72f37e4d612c25da42
Guillermo Roditi <groditi@gmail.com>
2009-12-27 21:32:17 -0500

    Cell changes

--- lib/Cantella/Data/Tabular/Cell.pm
+++ lib/Cantella/Data/Tabular/Cell.pm
@@ -1,11 +1,17 @@
 package Cantella::Data::Tabular::Cell;
 
 use Moose;
+use Cantella::Data::Tabular::Types qw( CellValue );
 
 has value => (
-  isa => 'Defined',
+  isa => CellValue,
   reader => 'get_value',
   writer => '_set_raw_value',
+  init_arg => undef,
+#  initializer => sub {
+#    my($self, $value, $callback, $attr) = @_;
+#    $callback->( $self->coerce_value_and_check_constraint($value) );
+#  },
   clearer => 'clear_value',
   predicate => 'has_value',
 );
@@ -19,24 +25,35 @@ has constraint => (
 has should_coerce => (
   is => 'rw',
   isa => 'Bool',
+  default => sub { 0 },
 );
 
+sub BUILD {
+  my($self, $args) = @_;
+  return unless exists $args->{value};
+  $self->set_value(delete $args->{value});
+}
 
 # I could have set the writer using:
 #  writer => { set_value => sub { ... } },
 # and I may, still, but I'm wondering whether it's too DWIMy or just right
 sub set_value {
+  my $self = shift;
+  $self->_set_raw_value( $self->coerce_value_and_check_constraint(@_) );
+}
+
+sub coerce_value_and_check_constraint {
   my ($self, $value) = @_;
   if( $self->has_constraint ){
     my $type_constraint = $self->constraint;
     if ($self->should_coerce && $type_constraint->has_coercion) {
       $value = $type_constraint->coerce($value);
     }
-    $type_constraint->check($value) ||
-      die( "Type constraint check failed: ".$type_constraint->get_message($value) );
+    unless ( $type_constraint->check($value) ) {
+      die("Type constraint check failed: ".$type_constraint->get_message($value));
+    }
   }
-
-  $self->_set_raw_value($value);
+  return $value;
 }
 
 __PACKAGE__->meta->make_immutable;
