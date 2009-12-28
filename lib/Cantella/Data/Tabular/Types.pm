ddefe611bbc54c9cc7692a4f895aa6dc7cd74222
Guillermo Roditi <groditi@gmail.com>
2009-12-27 21:33:14 -0500

    Types changes

--- lib/Cantella/Data/Tabular/Types.pm
+++ lib/Cantella/Data/Tabular/Types.pm
@@ -1,20 +1,37 @@
 package Cantella::Data::Tabular::Types;
 
-use Moose::Util::TypeConstraints;
+use MooseX::Types -declare => [qw(CellValue Cell Row)];
+use MooseX::Types::Moose qw(Defined);
 
-  subtype 'Natural'
-      => as 'Int'
-      => where { $_ > 0 };
+subtype CellValue, as Defined;
 
-  subtype 'NaturalLessThanTen'
-      => as 'Natural'
-      => where { $_ < 10 }
-      => message { "This number ($_) is not less than ten!" };
+class_type Cell, { class => 'Cantella::Data::Tabular::Cell' };
 
-  coerce 'Num'
-      => from 'Str'
-        => via { 0+$_ };
+class_type Row, { class => 'Cantella::Data::Tabular::Row' };
 
-  enum 'RGBColors' => qw(red green blue);
+1;
 
-  no Moose::Util::TypeConstraints;
+__END__;
+
+coerce Cell, from Defined, 
+  via { Cantella::Data::Tabular::Cell->new(value => $_) };
+
+coerce Cell, from HashRef, 
+  via { Cantella::Data::Tabular::Cell->new( %$_ ) };
+
+
+coerce Row, from ArrayRef,
+  via { 
+  my $row = Cantella::Data::Row->new;
+  $row->pad( $#{ $_ } );
+  for my $x ( 0 .. $#{ $_ } ){
+    next unless defined $_->[$x];
+    $row->set_value($x, $_->[$x]);
+  }
+  return row;
+};
+
+
+subtype RowCells, as ArrayRef[Cell];
+
+subtype TableRows, as ArrayRef[Row];
