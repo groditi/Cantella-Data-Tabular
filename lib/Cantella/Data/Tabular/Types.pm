663b910becf975d03401ae4075703b644f2c1048
Guillermo Roditi <groditi@gmail.com>
2009-12-31 17:29:21 -0500

    POD for Types

--- lib/Cantella/Data/Tabular/Types.pm
+++ lib/Cantella/Data/Tabular/Types.pm
@@ -13,15 +13,51 @@ class_type Row, { class => 'Cantella::Data::Tabular::Row' };
 
 __END__;
 
-coerce Cell, from Defined, 
+=head1 Cantella::Data::Tabular::Types
+
+Types Library for Cantella::Data::Tabular
+
+=head1 SYNOPSIS
+
+    use Cantella::Data::Tabular::Types qw( Cell );
+    has cell => ( isa => Cell,  ...);
+
+=head1 TYPES
+
+=head2 CellValue
+
+Subtype of 'Defined'
+
+=head2 Cell
+
+Class type for Cantella::Data::Tabular::Cell
+
+=head2 Row
+
+Class type for Cantella::Data::Tabular::Row
+
+=head1 AUTHOR
+
+Guillermo Roditi (groditi) E<lt>groditi@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2009 by Guillermo Roditi.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
+coerce Cell, from Defined,
   via { Cantella::Data::Tabular::Cell->new(value => $_) };
 
-coerce Cell, from HashRef, 
+coerce Cell, from HashRef,
   via { Cantella::Data::Tabular::Cell->new( %$_ ) };
 
 
 coerce Row, from ArrayRef,
-  via { 
+  via {
   my $row = Cantella::Data::Row->new;
   $row->pad( $#{ $_ } );
   for my $x ( 0 .. $#{ $_ } ){
